c71e76d9be47a294d530ac8035d4ea485c8bd0d9
aleksashin <vasilek.aleksashin@gmail.com>
2016-03-10 15:35:24 +0200

    Added getter and setter for currentDir field

--- Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/model/ShellPrompt.java
+++ Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/model/ShellPrompt.java
@@ -4,5 +4,7 @@ package t01.model;
  * Created by aleksashin on 09.03.2016.
  */
 public interface ShellPrompt {
+	String getCurrentDir();
+
 	String getPrompt();
 }
--- Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/model/impl/ShellPromptImpl.java
+++ Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/model/impl/ShellPromptImpl.java
@@ -4,18 +4,17 @@ import t01.model.ShellPrompt;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
 import java.util.Calendar;
 
 public class ShellPromptImpl implements ShellPrompt {
 	private final String user;
 	private final String host;
 	private final String home;
-	private final String work;
-	private final char[] separators = new char[]{'~', '@', '#', '%', '^', '&', '*', '[', ']', '<', '>', '-', '_', '=', ' ', '\n'};
 
-	private String startDir;
-	private int count = 1;
-	Calendar instance = Calendar.getInstance();
+	private String currentDir;
 
 	private String prompt;
 
@@ -29,9 +28,27 @@ public class ShellPromptImpl implements ShellPrompt {
 		}
 		this.host = hostName;
 		this.home = System.getProperty("user.home");
-		this.work = System.getProperty("user.dir");
-		this.startDir = this.home;
-		setPrompt(this.user, "@", this.host, " ", this.startDir, "\n", ">");
+		this.currentDir = this.home;
+		setPrompt(this.user, "@", this.host, " ", this.currentDir, "\n", ">");
+	}
+
+	@Override
+	public String getCurrentDir() {
+		return currentDir;
+	}
+
+	public void setCurrentDir(String currentDir) throws IllegalArgumentException {
+		if (currentDir == null) {
+			throw new IllegalArgumentException("Path can not be NULL");
+		}
+
+		try {
+			if (Files.exists(Paths.get(currentDir))) {
+                this.currentDir = currentDir;
+            }
+		} catch (InvalidPathException e) {
+			throw new IllegalArgumentException("Path not found");
+		}
 	}
 
 	@Override
