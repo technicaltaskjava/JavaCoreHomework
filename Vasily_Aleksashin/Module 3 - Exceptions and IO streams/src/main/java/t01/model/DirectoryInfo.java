b96ff7e94878e9506040318a9a5f3a99f8c12ba5
aleksashin <vasilek.aleksashin@gmail.com>
2016-03-11 00:09:07 +0200

    Extract the method of logical components

--- Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/model/DirectoryInfo.java
+++ Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/model/DirectoryInfo.java
@@ -1,5 +1,11 @@
 package t01.model;
 
+import t01.exception.ModelException;
+
+import java.nio.file.Path;
+
 public interface DirectoryInfo {
-    String getDirInfo(String path) throws IllegalArgumentException;
+	Path[] getDirStream(String path) throws ModelException;
+
+	String getDirInfo(Path[] paths) throws ModelException;
 }
--- Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/model/impl/DirectoryInfoImpl.java
+++ Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/model/impl/DirectoryInfoImpl.java
@@ -1,5 +1,6 @@
 package t01.model.impl;
 
+import t01.exception.ModelException;
 import t01.model.DirectoryInfo;
 
 import java.io.File;
@@ -7,23 +8,39 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.FileTime;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 
 public class DirectoryInfoImpl implements DirectoryInfo {
-    private DirectoryStream<Path> dirStream;
     private int countFile = 0;
     private int countDir = 0;
     private long sizeFile = 0;
 
-    @Override
-    public String getDirInfo(final String path) throws IllegalArgumentException {
-        if (path == null) {
-            throw new IllegalArgumentException("Path can not be NULL");
-        }
+	@Override
+    public Path[] getDirStream(final String path) throws ModelException {
+	    validate(path);
 
+	    Path[] paths = null;
+	    try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(Paths.get(path))){
+		    for (Path element : dirStream) {
+			    if (paths == null) {
+				    paths = new Path[1];
+			    } else {
+				    paths = Arrays.copyOf(paths, paths.length);
+			    }
+			    paths[paths.length - 1] = element;
+		    }
+	    } catch (IOException e) {
+		    e.printStackTrace();
+	    }
+	    return paths;
+	}
+
+	@Override
+    public String getDirInfo(final Path[] paths) throws ModelException {
+		validate(paths);
         StringBuilder builder = new StringBuilder();
         try {
-            dirStream = Files.newDirectoryStream(Paths.get(path));
-            for (Path dir : dirStream) {
+            for (Path dir : paths) {
                 File file = new File(dir.toString());
                 builder.append(formatterTime(dir)).append("\t");
                 if (file.isDirectory()) {
@@ -39,12 +56,18 @@ public class DirectoryInfoImpl implements DirectoryInfo {
             builder.append("\t\t\t\t").append(countDir).append(" directories");
             builder.append("\n\t\t\t\t").append(countFile).append(" files ").append(String.format("%,8d", sizeFile)).append(" byte");
         } catch (InvalidPathException e) {
-            throw new IllegalArgumentException("Path not found");
+            throw new ModelException("Path not found");
         } catch (IOException e) {
-            throw new IllegalArgumentException("Can not access to path: " + path);
+            throw new ModelException("Can not access to path");
         }
         return builder.toString();
     }
+
+	private void validate(final Object obj) throws ModelException {
+		if (obj == null) {
+			throw new ModelException("Path can not be NULL");
+		}
+	}
 
     private String formatterTime(Path dir) throws IOException {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd.mm.yyyy  HH.mm.ss");
