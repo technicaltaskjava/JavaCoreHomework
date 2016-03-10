3e341382ba0f71a4afad4a6fb593bcb7d170e087
aleksashin <vasilek.aleksashin@gmail.com>
2016-03-10 15:18:53 +0200

    Added method to get directory information

---
+++ Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/model/DirectorySearch.java
@@ -0,0 +1,5 @@
+package t01.model;
+
+public interface DirectorySearch {
+    String getFullTable(String path) throws IllegalArgumentException;
+}
--- Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/model/impl/DirectorySearchImpl.java
+++ Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/model/impl/DirectorySearchImpl.java
@@ -1,5 +1,69 @@
 package t01.model.impl;
 
-public class DirectorySearchImpl {
+import t01.model.DirectorySearch;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.FileTime;
+import java.text.SimpleDateFormat;
+
+public class DirectorySearchImpl implements DirectorySearch {
+    private DirectoryStream<Path> dirStream;
+    private int countFile = 0;
+    private int countDir = 0;
+    private long sizeFile = 0;
+
+    @Override
+    public String getFullTable(final String path) throws IllegalArgumentException {
+        if (path == null) {
+            throw new IllegalArgumentException("Path can not be NULL");
+        }
+
+        StringBuilder builder = new StringBuilder();
+        try {
+            dirStream = Files.newDirectoryStream(Paths.get(path));
+            for (Path dir : dirStream) {
+                File file = new File(dir.toString());
+                builder.append(getFormatterTime(dir)).append("\t");
+                if (file.isDirectory()) {
+                    builder.append("<DIR>").append("\t\t");
+                    countDir++;
+                } else {
+                    builder.append(alignFileSize(dir)).append(String.format("%,8d", Files.size(dir)));
+                    countFile++;
+                    sizeFile += Files.size(dir);
+                }
+                builder.append("\t").append(file.getName()).append("\n");
+            }
+            builder.append("\t\t\t\t").append(countDir).append(" directories");
+            builder.append("\n\t\t\t\t").append(countFile).append(" files ").append(String.format("%,8d", sizeFile)).append(" byte");
+        } catch (InvalidPathException e) {
+            throw new IllegalArgumentException("Path not found");
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Can not access to path: " + path);
+        } finally {
+            try {
+                dirStream.close();
+            } catch (IOException e) {
+                throw new IllegalArgumentException(e.getMessage());
+            }
+        }
+        return builder.toString();
+    }
+
+    private String getFormatterTime(Path dir) throws IOException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.mm.yyyy  HH.mm.ss");
+        FileTime fileTime = Files.getLastModifiedTime(dir);
+        return dateFormat.format(fileTime.toMillis());
+    }
+
+    private String alignFileSize(Path dir) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        int length = String.format("%,8d", Files.size(dir)).length();
+        for (int index = 0; index < 15 - length; index++) {
+            builder.append(" ");
+        }
+        return builder.toString();
+    }
 }
