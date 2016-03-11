be3738bee500f4350304908a7bd85e048c6fd4f9
aleksashin <vasilek.aleksashin@gmail.com>
2016-03-11 15:59:41 +0200

    Added command "help" with test

---
+++ Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/controller/command/impl/Help.java
@@ -0,0 +1,32 @@
+package t01.controller.command.impl;
+
+import t01.controller.MainController;
+import t01.controller.command.Command;
+import t01.exception.ExitException;
+
+public class Help implements Command {
+    private static final String cmdName = "help";
+
+    private final MainController controller;
+
+    public Help(MainController controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public boolean canExecute(String input) {
+        return input != null && input.equals(cmdName);
+    }
+
+    @Override
+    public void execute(String input) throws ExitException {
+        String msg = "Available Commands:\n" +
+                "dir\n\tOutput the contents of the current directory to the screen\n" +
+                "cd\n\tSyntax: cd [path]" +
+                "\n\t\t.. indicates the parent directory.  Example: cd ..      - move up" +
+                "\n\t\t.  indicates the current directory. Example: cd .\\dir  - goto dir in current path" +
+                "\n\tChange the current directory\n" +
+                "exit\n\tExit from application";
+        controller.print(msg);
+    }
+}
