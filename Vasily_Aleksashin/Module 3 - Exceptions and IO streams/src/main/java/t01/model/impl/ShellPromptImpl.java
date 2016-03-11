cdb822ed28c3d119ec314a3921f05ffca6ad4337
aleksashin <vasilek.aleksashin@gmail.com>
2016-03-11 09:10:22 +0200

    Formatted code

--- Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/model/impl/ShellPromptImpl.java
+++ Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/model/impl/ShellPromptImpl.java
@@ -9,58 +9,58 @@ import java.nio.file.InvalidPathException;
 import java.nio.file.Paths;
 
 public class ShellPromptImpl implements ShellPrompt {
-	public static ShellPrompt instance = null;
+    public static ShellPrompt instance = null;
 
-	private String currentDir;
-	private String prompt;
+    private String currentDir;
+    private String prompt;
 
-	private ShellPromptImpl() {
-		currentDir = Environment.getHomeDir();
-		setPrompt(Environment.getUserName(), "@", Environment.getHostName(), " ", currentDir, "\n", ">");
-	}
+    private ShellPromptImpl() {
+        currentDir = Environment.getHomeDir();
+        setPrompt(Environment.getUserName(), "@", Environment.getHostName(), " ", currentDir, "\n", ">");
+    }
 
-	public static ShellPrompt getInstance() {
-		if (instance == null) {
-			instance = new ShellPromptImpl();
-		}
-		return instance;
-	}
-
-	@Override
-	public String getCurrentDir() {
-		return currentDir;
-	}
+    public static ShellPrompt getInstance() {
+        if (instance == null) {
+            instance = new ShellPromptImpl();
+        }
+        return instance;
+    }
 
     @Override
-	public void setCurrentDir(String path) throws ModelException {
-		if (path == null) {
-			throw new ModelException("Path can not be NULL");
-		}
+    public String getCurrentDir() {
+        return currentDir;
+    }
+
+    @Override
+    public void setCurrentDir(String path) throws ModelException {
+        if (path == null) {
+            throw new ModelException("Path can not be NULL");
+        }
         if (path.equals("")) {
             throw new ModelException("Path can not be EMPTY");
         }
-		try {
-			if (Files.exists(Paths.get(path))) {
-				this.currentDir = path;
-			}
-		} catch (InvalidPathException e) {
-			throw new ModelException(String.format("Path: '%s' not found", path));
-		}
-	}
+        try {
+            if (Files.exists(Paths.get(path))) {
+                this.currentDir = path;
+            }
+        } catch (InvalidPathException e) {
+            throw new ModelException(String.format("Path: '%s' not found", path));
+        }
+    }
 
-	@Override
-	public String getPrompt() {
-		return prompt;
-	}
+    @Override
+    public String getPrompt() {
+        return prompt;
+    }
 
-	private void setPrompt(String... args) {
-		if (args != null && args.length > 0) {
-			StringBuilder builder = new StringBuilder();
-			for (String arg : args) {
-				builder.append(arg);
-			}
-			builder.append(" ");
-			prompt = builder.toString();
-		}
-	}
+    private void setPrompt(String... args) {
+        if (args != null && args.length > 0) {
+            StringBuilder builder = new StringBuilder();
+            for (String arg : args) {
+                builder.append(arg);
+            }
+            builder.append(" ");
+            prompt = builder.toString();
+        }
+    }
 }
