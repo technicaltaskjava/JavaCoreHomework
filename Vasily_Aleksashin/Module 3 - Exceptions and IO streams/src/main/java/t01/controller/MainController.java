d37dda80f55b2689b523489127d1175f345fa4fa
aleksashin <vasilek.aleksashin@gmail.com>
2016-03-11 16:05:09 +0200

    Added available commands

--- Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/controller/MainController.java
+++ Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/controller/MainController.java
@@ -1,8 +1,10 @@
 package t01.controller;
 
 import t01.controller.command.Command;
+import t01.controller.command.impl.Cd;
 import t01.controller.command.impl.Dir;
 import t01.controller.command.impl.Exit;
+import t01.controller.command.impl.Help;
 import t01.exception.ExitException;
 import t01.model.ShellPrompt;
 import t01.model.impl.ShellPromptImpl;
@@ -10,27 +12,34 @@ import t01.view.View;
 import t01.view.impl.ConsoleViewImpl;
 
 public class MainController {
-	private ShellPrompt prompt;
-	private View view;
-	private Command[] commands;
+    private View view;
+    private Command[] commands;
 
-	public void run() throws ExitException {
-		init();
-		while (true) {
-			view.print("");
-			String input = view.read();
-			for (Command cmd : commands) {
-				cmd.execute(input);
-			}
-		}
-	}
+    public void run() throws ExitException {
+        init();
+        view.print("The application does not recognize Cyrillic\nEnter command or help");
+        while (true) {
+            String input = view.read();
+            boolean flag = false;
+            for (Command cmd : commands) {
+                if (cmd.canExecute(input)) {
+                    flag = true;
+                    cmd.execute(input);
+                }
+            }
+            if (!flag) {
+                view.print("\tEnter wrong command");
+            }
+        }
+    }
 
-	public void print(final String output) {
-		view.print(output);
-	}
-	private void init() {
-		prompt = ShellPromptImpl.getInstance();
-		view = new ConsoleViewImpl(prompt);
-		commands = new Command[] {new Exit(), new Dir(this)};
-	}
+    public void print(final String output) {
+        view.print(output);
+    }
+
+    private void init() {
+        ShellPrompt prompt = ShellPromptImpl.getInstance();
+        view = new ConsoleViewImpl(prompt);
+        commands = new Command[]{new Exit(), new Dir(this), new Cd(this), new Help(this)};
+    }
 }
