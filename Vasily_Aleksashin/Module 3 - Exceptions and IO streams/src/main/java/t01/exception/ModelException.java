0bf3f9af4e651c4e6b7e73f45907aaaee1ccac28
aleksashin <vasilek.aleksashin@gmail.com>
2016-03-11 16:03:54 +0200

    Remove unused constructor

--- Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/exception/ModelException.java
+++ Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/exception/ModelException.java
@@ -4,8 +4,4 @@ public class ModelException extends Exception {
 	public ModelException(final String message) {
 		super(message);
 	}
-
-	public ModelException(final String message, final Throwable cause) {
-		super(message, cause);
-	}
 }
