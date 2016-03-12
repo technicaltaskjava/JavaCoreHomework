af3e08ae80e2e458a9eb42041f8aed46bfc2a92a
aleksashin <vasilek.aleksashin@gmail.com>
2016-03-12 18:39:10 +0200

    Added custom exception

---
+++ Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t02/exception/ParameterNullException.java
@@ -0,0 +1,7 @@
+package t02.exception;
+
+public class ParameterNullException extends Exception{
+	public ParameterNullException(final String message) {
+		super(message);
+	}
+}
---
+++ Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t02/exception/PropertyNotFoundException.java
@@ -0,0 +1,7 @@
+package t02.exception;
+
+public class PropertyNotFoundException extends Exception {
+	public PropertyNotFoundException(final String message) {
+		super(message);
+	}
+}
