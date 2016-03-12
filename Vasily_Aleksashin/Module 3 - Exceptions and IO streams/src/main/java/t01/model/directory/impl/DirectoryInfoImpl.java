bd962df5cf54a8fee39a546a13c155364342a8cc
aleksashin <vasilek.aleksashin@gmail.com>
2016-03-12 16:22:15 +0200

    Reformat code

--- Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/model/directory/impl/DirectoryInfoImpl.java
+++ Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/model/directory/impl/DirectoryInfoImpl.java
@@ -15,74 +15,74 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 public class DirectoryInfoImpl implements DirectoryInfo {
-    private int countFile = 0;
-    private int countDir = 0;
-    private long sizeFile = 0;
+	private int countFile = 0;
+	private int countDir = 0;
+	private long sizeFile = 0;
 
-    @Override
-    public Path[] getDirStream(final String path) throws ModelException {
-        Path preparedPath = Environment.preparedPath(path);
-        Path[] paths = null;
-        try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(preparedPath)) {
-            for (Path element : dirStream) {
-                if (paths == null) {
-                    paths = new Path[1];
-                } else {
-                    paths = Arrays.copyOf(paths, paths.length + 1);
-                }
-                paths[paths.length - 1] = element;
-            }
-        } catch (IOException e) {
-            throw new ModelException("Path not found");
-        }
-        return paths;
-    }
+	@Override
+	public Path[] getDirStream(final String path) throws ModelException {
+		Path preparedPath = Environment.preparedPath(path);
+		Path[] paths = null;
+		try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(preparedPath)) {
+			for (Path element : dirStream) {
+				if (paths == null) {
+					paths = new Path[1];
+				} else {
+					paths = Arrays.copyOf(paths, paths.length + 1);
+				}
+				paths[paths.length - 1] = element;
+			}
+		} catch (IOException e) {
+			throw new ModelException("Path not found");
+		}
+		return paths;
+	}
 
-    @Override
-    public String getDirInfo(final Path[] paths) throws ModelException {
-        if (paths == null) {
-            throw new ModelException("Path can not be NULL");
-        }
-        StringBuilder builder = new StringBuilder();
-        try {
-            for (Path dir : paths) {
-                File file = new File(dir.toString());
-	            if (!file.isHidden()) {
-		            builder.append(formatterTime(dir)).append("\t");
-		            if (file.isDirectory()) { //TODO check LINK
-		                builder.append("<DIR>").append("\t\t");
-		                countDir++;
-		            } else {
-		                builder.append(alignFileSize(dir));
-		                countFile++;
-		                sizeFile += Files.size(dir);
-		            }
-		            builder.append("\t").append(file.getName()).append("\n");
-	            }
-            }
-            builder.append("\t\t\t\t").append(countDir).append(" directories");
-            builder.append("\n\t\t\t\t").append(countFile).append(" files ").append(String.format("%,8d", sizeFile)).append(" byte");
-        } catch (InvalidPathException e) {
-            throw new ModelException("Path not found");
-        } catch (IOException e) {
-            throw new ModelException("Can not access to path");
-        }
-        return builder.toString();
-    }
+	@Override
+	public String getDirInfo(final Path[] paths) throws ModelException {
+		if (paths == null) {
+			throw new ModelException("Path can not be NULL");
+		}
+		StringBuilder builder = new StringBuilder();
+		try {
+			for (Path dir : paths) {
+				File file = new File(dir.toString());
+				if (!file.isHidden()) {
+					builder.append(formatterTime(dir)).append("\t");
+					if (file.isDirectory()) {
+						builder.append("<DIR>").append("\t\t");
+						countDir++;
+					} else {
+						builder.append(alignFileSize(dir));
+						countFile++;
+						sizeFile += Files.size(dir);
+					}
+					builder.append("\t").append(file.getName()).append("\n");
+				}
+			}
+			builder.append("\t\t\t\t").append(countDir).append(" directories");
+			builder.append("\n\t\t\t\t").append(countFile).append(" files ").append(String.format("%,8d", sizeFile)).append(" byte");
+		} catch (InvalidPathException e) {
+			throw new ModelException("Path not found");
+		} catch (IOException e) {
+			throw new ModelException("Can not access to path");
+		}
+		return builder.toString();
+	}
 
-    private String formatterTime(Path dir) throws IOException {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy - HH.mm.ss");
-        FileTime fileTime = Files.getLastModifiedTime(dir);
-        return dateFormat.format(fileTime.toMillis());
-    }
+	private String formatterTime(Path dir) throws IOException {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy - HH.mm.ss");
+		FileTime fileTime = Files.getLastModifiedTime(dir);
+		return dateFormat.format(fileTime.toMillis());
+	}
 
-    private String alignFileSize(Path dir) throws IOException {
-        StringBuilder builder = new StringBuilder();
-        int length = String.format("%,8d", Files.size(dir)).length();
-        for (int index = 0; index < 15 - length; index++) {
-            builder.append(" ");
-        }
-        builder.append(String.format("%,8d", Files.size(dir)));
-        return builder.toString();
-    }
+	private String alignFileSize(Path dir) throws IOException {
+		StringBuilder builder = new StringBuilder();
+		int length = String.format("%,8d", Files.size(dir)).length();
+		for (int index = 0; index < 15 - length; index++) {
+			builder.append(" ");
+		}
+		builder.append(String.format("%,8d", Files.size(dir)));
+		return builder.toString();
+	}
 }
