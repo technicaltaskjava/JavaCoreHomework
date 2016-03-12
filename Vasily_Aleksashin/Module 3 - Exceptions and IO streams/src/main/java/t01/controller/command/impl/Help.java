c0602ebe7948d48436913a435fafcc9bda0d6a57
aleksashin <vasilek.aleksashin@gmail.com>
2016-03-12 14:46:09 +0200

    Added help for new commands

--- Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/controller/command/impl/Help.java
+++ Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/controller/command/impl/Help.java
@@ -21,11 +21,23 @@ public class Help implements Command {
     @Override
     public void execute(String input) throws ExitException {
         String msg = "Available Commands:\n" +
-                "dir\n\tOutput the contents of the current directory to the screen\n" +
+                "dir\n\tOutput the contents from the current directory to the screen\n" +
                 "cd\n\tSyntax: cd [path]" +
+		        "\n\t\t[path] relative path to the target directory from the current directory" +
                 "\n\t\t.. indicates the parent directory.  Example: cd ..      - move up" +
                 "\n\t\t.  indicates the current directory. Example: cd .\\dir  - goto dir in current path" +
                 "\n\tChange the current directory\n" +
+		        "newFile\n\tSyntax: newFile [[path]fileName]" +
+		        "\n\t\t[path] relative path to the file from the current directory" +
+		        "\n\tCreate new empty file in current directory\n" +
+		        "read\n\tSyntax: read [[path]fileName]" +
+		        "\n\t\t[path] relative path to the file from the current directory" +
+		        "\n\tRead file in current directory\n" +
+		        ">\n\tSyntax: [text] > [[path]fileName] [-a]" +
+		        "\n\t\t[text] text that should be added to the file" +
+		        "\n\t\t[path] relative path to the file from the current directory" +
+		        "\n\t\t[-a]  command-line option, which performs the recording the text to the end of the file" +
+		        "\n\tWrite file in current directory\n" +
                 "exit\n\tExit from application";
         controller.print(msg);
     }
