52f7c2f0893e6469816aedd9eab1e51a635135eb
aleksashin <vasilek.aleksashin@gmail.com>
2016-03-12 11:57:50 +0200

    Added read/write file methods with TDD and file for testing

--- Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/model/file/FileOperations.java
+++ Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/model/file/FileOperations.java
@@ -6,4 +6,8 @@ public interface FileOperations {
 	void create(String fileName) throws ModelException;
 
 	void delete(String fileName) throws ModelException;
+
+	String read(String fileName) throws ModelException;
+
+	void write(String fileName, String message, boolean append) throws ModelException;
 }
--- Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/model/file/impl/FileOperationsImpl.java
+++ Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t01/model/file/impl/FileOperationsImpl.java
@@ -3,13 +3,12 @@ package t01.model.file.impl;
 import t01.exception.ModelException;
 import t01.model.file.FileOperations;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class FileOperationsImpl implements FileOperations {
 	@Override
 	public void create(final String fileName) throws ModelException {
-		validateFileName(fileName);
+		validateParameter(fileName);
 
 		File file = new File(fileName);
 		try {
@@ -23,17 +22,60 @@ public class FileOperationsImpl implements FileOperations {
 
 	@Override
 	public void delete(final String fileName) throws ModelException {
-		validateFileName(fileName);
+		validateParameter(fileName);
 
 		File file = new File(fileName);
-		if (!file.delete()) {
+		if (!validateFile(file) || !file.delete()) {
 			throw new ModelException(String.format("Can not delete file %s", fileName));
 		}
 	}
 
-	private void validateFileName(final String fileName) throws ModelException {
-		if (fileName == null || fileName.equals("")) {
-			throw new ModelException("File name incorrect");
+	@Override
+	public String read(final String fileName) throws ModelException {
+		validateParameter(fileName);
+		File file = new File(fileName);
+		if (!file.isFile() || !file.canRead()) {
+			throw new ModelException(String.format("Can not read file %s", fileName));
+		}
+		StringBuilder builder = new StringBuilder();
+		String cache;
+		try (FileReader in = new FileReader(file);
+		     BufferedReader reader = new BufferedReader(in)) {
+			while ((cache = reader.readLine()) != null) {
+				builder.append(cache);
+			}
+		} catch (IOException e) {
+			throw new ModelException(e.getMessage());
+		}
+
+		return builder.toString();
+	}
+
+	@Override
+	public void write(final String fileName, final String message, final boolean append) throws ModelException {
+		validateParameter(fileName);
+		validateParameter(message);
+		File file = new File(fileName);
+		if (!validateFile(file)) {
+			throw new ModelException(String.format("Can not write file %s", fileName));
+		}
+		try (FileWriter out = new FileWriter(fileName, append);
+		     BufferedWriter writer = new BufferedWriter(out)) {
+			writer.write(message);
+		} catch (IOException e) {
+			throw new ModelException(e.getMessage());
 		}
 	}
+
+	private void validateParameter(final String fileName) throws ModelException {
+		if (fileName == null || fileName.equals("")) {
+			throw new ModelException("Parameter incorrect");
+		}
+	}
+
+	private boolean validateFile(final File file) {
+		return file.isFile() && file.canWrite();
+	}
+
+
 }
