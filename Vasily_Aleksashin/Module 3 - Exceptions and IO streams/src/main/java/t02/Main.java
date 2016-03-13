cbaef7f105a40c3dee7f4e0246ebe8437c505205
aleksashin <vasilek.aleksashin@gmail.com>
2016-03-13 20:08:45 +0200

    Added main menu

--- Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t02/Main.java
+++ Vasily_Aleksashin/Module 3 - Exceptions and IO streams/src/main/java/t02/Main.java
@@ -1,16 +1,15 @@
 package t02;
 
-import t01.exception.ModelException;
+import t01.exception.ExitException;
 import t02.controller.MainController;
 
 public class Main {
 	public static void main(String[] args) {
-		String file = "C:\\Users\\aleksashin\\IdeaProjects\\JavaCoreHomework\\Vasily_Aleksashin\\Module 3 - Exceptions and IO streams\\src\\main\\resources\\various.properties";
 		MainController controller = new MainController();
 		try {
-			controller.load(file);
-		} catch (ModelException e) {
-			System.out.println(e.getMessage());
+			controller.run(args);
+		} catch (ExitException e) {
+			System.out.println("Goodbye");
 		}
 	}
 }
