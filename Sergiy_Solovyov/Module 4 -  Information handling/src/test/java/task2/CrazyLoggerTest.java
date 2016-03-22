233541a83b0fa295dcfe31830923ec216efeb1bc
Nightingale85 <solovyov85@gmail.com>
2016-03-22 13:54:41 +0200

    Sergey Solovyov Module 4: Information handling

---
+++ Sergiy_Solovyov/Module 4 -  Information handling/src/test/java/task2/CrazyLoggerTest.java
@@ -0,0 +1,22 @@
+package task2;
+
+import org.junit.Test;
+import task3.HTMLParser;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Sergey Solovyov
+ * @version 1.0
+ * @since 21.03.2016
+ */
+public class CrazyLoggerTest {
+    @Test
+    public void testParseHTML()   {
+
+    }
+}
