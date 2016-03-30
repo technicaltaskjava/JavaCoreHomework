61d9f7b92b81e453e5a335adffda7b1aa8fa3108
education2016-Kokhanyuk <akoh@i.ua>
2016-03-30 16:32:56 +0300

    Oleksander Kokhanyuk: Module 5

---
+++ Oleksander_Kokhanyuk/Module 5 - Annotations and Generics/src/main/java/com/kokhanyuk/javase05/myjunit/NegativeRadiusException.java
@@ -0,0 +1,13 @@
+package com.kokhanyuk.javase05.myjunit;
+
+
+/**
+ * NegativeRadiusException
+ *
+ * This class describes the exemption for the negative radius.
+ *
+ * @author Olexandr Kokhanyuk
+ * @version 1.0
+ */
+public class NegativeRadiusException extends Exception{
+}
