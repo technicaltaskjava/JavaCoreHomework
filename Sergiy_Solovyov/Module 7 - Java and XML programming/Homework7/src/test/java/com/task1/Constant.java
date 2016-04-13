07156ac7de064096d17907df9a92c9673fbf5eef
Nightingale85 <solovyov85@gmail.com>
2016-04-13 13:48:13 +0300

    fix sonar

--- Sergiy_Solovyov/Module 7 - Java and XML programming/Homework7/src/test/java/com/task1/Constant.java
+++ Sergiy_Solovyov/Module 7 - Java and XML programming/Homework7/src/test/java/com/task1/Constant.java
@@ -1,7 +1,6 @@
 package com.task1;
 
 public class Constant {
-    private Constant(){}
 
     public static final String RESULT = "---------------------------------\n" +
             "CLEOPATRA\n" +
@@ -13,4 +12,5 @@ public class Constant {
             "replicas - 1\n" +
             "average quantity words in replica - 11,00\n" +
             "words in all play  - 11\n";
+    private Constant(){}
 }
