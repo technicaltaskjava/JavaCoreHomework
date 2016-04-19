8d508bd046d65a86a342ee30de9b5e29014c0133
Oleg Gondar <_oleg@ua.fm>
2016-04-20 02:27:45 +0300

    Oleg Gondar Homework Module 8 - Multitreading

---
+++ Oleg_Gondar/Module 8 - MultiThreading/src/main/java/task2/AccountFileReader.java
@@ -0,0 +1,26 @@
+package task2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by Oleg on 20.04.2016.
+ */
+public class AccountFileReader {
+
+    public static List<String> getLines(String fileName) throws FileNotFoundException {
+
+        List<String> accountsOperations = new LinkedList<String>();
+        Scanner sc = new Scanner(new File(fileName));
+        while (sc.hasNext()){
+            accountsOperations.add(sc.nextLine());
+        }
+        sc.close();
+        return accountsOperations;
+    }
+
+
+}
