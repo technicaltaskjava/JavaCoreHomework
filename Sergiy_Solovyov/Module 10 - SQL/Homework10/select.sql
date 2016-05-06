96b5fe427e32fdfa84a42d26d300847a1c07c0e4
Nightingale85 <solovyov85@gmail.com>
2016-05-06 18:37:01 +0300

    add ; to end of request

--- Sergiy_Solovyov/Module 10 - SQL/Homework10/select.sql
+++ Sergiy_Solovyov/Module 10 - SQL/Homework10/select.sql
@@ -1,12 +1,12 @@
-SELECT * from COOKIES 
+SELECT * from COOKIES; 
 
-SELECT * from METADATA 
+SELECT * from METADATA; 
 
-SELECT * from USERS 
+SELECT * from USERS; 
 
-SELECT * FROM USERS where age between 36 and 99 order by age
+SELECT * FROM USERS where age between 36 and 99 order by age;
 
-SELECT * FROM USERS where username like 'M%'
+SELECT * FROM USERS where username like 'M%';
 
 SELECT * FROM COOKIES  WHERE LENGTH(cookie) > 70;
 
@@ -18,16 +18,16 @@ WHERE metadata.cookie_id=cookies.id AND metadata.user_id=users.id
 ORDER BY users.age;
 
 select USERNAME, EMAIL  from USERS
-where age between 40 and 80 or length(PASSWORD) > 4
+where age between 40 and 80 or length(PASSWORD) > 4;
 
 select USERNAME, EMAIL  from USERS where id > 5
 union
-select USERNAME, EMAIL  from USERS where age < 50
+select USERNAME, EMAIL  from USERS where age < 50;
 
 select TIME_ADDED, COOKIE 
 from metadata inner join cookies  
-on  metadata .COOKIE_ID =cookies .ID 
+on  metadata .COOKIE_ID =cookies .ID;
 
-select USERNAME , PASSWORD  from USERS where id in (2,6)
+select USERNAME , PASSWORD  from USERS where id in (2,6);
 
-select max(age) as MAX_AGE from users
+select max(age) as MAX_AGE from users;
