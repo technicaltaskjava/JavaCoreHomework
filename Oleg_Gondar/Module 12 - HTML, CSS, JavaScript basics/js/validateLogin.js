76c18e6b1cad7936cc10c4ff9f9dd8d35b00c178
GondarOleg <_oleg@ua.fm>
2016-05-18 10:19:19 +0300

    Oleg Gondar Homework Module  12 - HTML, CSS, JavaScript basics

--- Oleg_Gondar/Module 12 - HTML, CSS, JavaScript basics/js/validateLogin.js
+++ Oleg_Gondar/Module 12 - HTML, CSS, JavaScript basics/js/validateLogin.js
@@ -1,43 +1,45 @@
-$(document).ready(function() {
-    $("#login-button").click(function(e){
-      var allValid = true;
-      function validateName(value) {
-          if (value && value.length > 1) {
-              return false;
-          }
+$(document).ready(function () {
+    $("#login-button").click(function (e) {
+        var allValid = true;
 
-          return true;
-      }
+        function validateName(value) {
+            if (value && value.length > 1) {
+                return false;
+            }
+
+            return true;
+        }
 
 
-      function validatePassword(value) {
-          if (value && value.length > 5) {
-              return false;
-          }
+        function validatePassword(value) {
+            if (value && value.length > 5) {
+                return false;
+            }
 
-          return true;
-      }
+            return true;
+        }
+
         e.preventDefault();
-        if(validateName($('#username').val())){
-         $("#errors").append("<p>Error in username, must be 3 characters at least</p>");
-         allValid = false;
-      }
-      if(validatePassword($('#password').val())){
-       $("#errors").append("<p>Error in password, must be 6 characters at least</p>");
-       allValid = false;
-    }
-      if(allValid){
-        // alert("All good!!");
-        var win = window.open('cookiesTable.html', '_blank');
-if(win){
-    //Browser has allowed it to be opened
-    win.focus();
-}else{
-    //Broswer has blocked it
-    alert('Please allow popups for this site');
-}
+        if (validateName($('#username').val())) {
+            $("#errors").append("<p>Error in username, must be 3 characters at least</p>");
+            allValid = false;
+        }
+        if (validatePassword($('#password').val())) {
+            $("#errors").append("<p>Error in password, must be 6 characters at least</p>");
+            allValid = false;
+        }
+        if (allValid) {
+            // alert("All good!!");
+            var win = window.open('cookiesTable.html', '_blank');
+            if (win) {
+                //Browser has allowed it to be opened
+                win.focus();
+            } else {
+                //Broswer has blocked it
+                alert('Please allow popups for this site');
+            }
 
-      }
+        }
 
     });
 });
--- Oleg_Gondar/Module 12 - HTML, CSS, JavaScript basics/js/validateRegister.js
+++ Oleg_Gondar/Module 12 - HTML, CSS, JavaScript basics/js/validateRegister.js
@@ -1,58 +1,60 @@
-$(document).ready(function() {
-    $("#register").click(function(e){
-      var allValid = true;
-      function validateName(value) {
-          if (value && value.length > 1) {
-              return false;
-          }
+$(document).ready(function () {
+    $("#register").click(function (e) {
+        var allValid = true;
 
-          return true;
-      }
+        function validateName(value) {
+            if (value && value.length > 1) {
+                return false;
+            }
 
-      function validateEmail(value) {
-              var regexp = /^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@([a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$/;
+            return true;
+        }
 
-              if (value && regexp.test(value)) {
-                  return false;
-              }
+        function validateEmail(value) {
+            var regexp = /^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@([a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$/;
 
-              return true;
-          }
+            if (value && regexp.test(value)) {
+                return false;
+            }
+
+            return true;
+        }
 
 
-      function validatePassword(value) {
-          if (value && value.length > 5) {
-              return false;
-          }
+        function validatePassword(value) {
+            if (value && value.length > 5) {
+                return false;
+            }
 
-          return true;
-      }
+            return true;
+        }
+
         e.preventDefault();
-        if(validateName($('#username').val())){
-         $("#errors").append("<p>Error in username, must be 3 characters at least</p>");
-         allValid = false;
-      }
-      if(validatePassword($('#password').val())){
-       $("#errors").append("<p>Error in password, must be 6 characters at least</p>");
-       allValid = false;
-    }
-    if(validateEmail($('#email').val())){
-     $("#errors").append("<p>Error in email, must be as address@some.domain</p>");
-     allValid = false;
-  }
+        if (validateName($('#username').val())) {
+            $("#errors").append("<p>Error in username, must be 3 characters at least</p>");
+            allValid = false;
+        }
+        if (validatePassword($('#password').val())) {
+            $("#errors").append("<p>Error in password, must be 6 characters at least</p>");
+            allValid = false;
+        }
+        if (validateEmail($('#email').val())) {
+            $("#errors").append("<p>Error in email, must be as address@some.domain</p>");
+            allValid = false;
+        }
 
-  if(allValid){
-    // alert("All good!!");
-    var win = window.open('cookiesTable.html', '_blank');
-if(win){
-//Browser has allowed it to be opened
-win.focus();
-}else{
-//Broswer has blocked it
-alert('Please allow popups for this site');
-}
+        if (allValid) {
+            // alert("All good!!");
+            var win = window.open('cookiesTable.html', '_blank');
+            if (win) {
+                //Browser has allowed it to be opened
+                win.focus();
+            } else {
+                //Broswer has blocked it
+                alert('Please allow popups for this site');
+            }
 
-  }
+        }
 
-});
+    });
 });
